fa8957923347f08851d7d2b27fb613a8e95f1a2a
Derrick Nozumi <teryakiasian@gmail.com>
2015-08-31 15:55:58 -0600

    Variables and Functions

---
+++ Experiment Unity project/Assets/Pass_off.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pass_off : MonoBehaviour
+{
+	int MyFun = 25;
+	// Use this for initialization
+	void Start () {
+	
+		MyFun = MultiplyByFour (MyFun);
+		Debug.Log (MyFun);
+	}
+		int MultiplyByFour (int number){
+			int ret;
+			                       
+			ret = number * 4;
+
+			return ret;
+	
+	}
+}
