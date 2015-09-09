eca92db2c3c9be720a27bba76172ba087cc08889
Derrick Nozumi <10596306@uvu.edu>
2015-09-09 15:08:14 -0600

    Conventions and Syntax debug

---
+++ Syntax debug/Assets/Syntax_debug.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyScript : MonoBehaviour 
+	{
+	private float heightTest = 3;
+	void Start ()
+	{
+		Debug.Log(transform.position.y);
+		if(transform.position.y < heightTest)
+		{
+			Debug.Log("I'm about to hit the ground!");
+			FlyAway();
+		}
+		else 
+		{
+			if (transform.position.y > heightTest)
+			{
+				Debug.Log("I'm flying!");
+			}
+		}
+	}
+	private void FlyAway()
+	{
+		Debug.Log("Fly Away!");
+		Vector3 newposition = new Vector3(0,7,0);
+		transform.position = newposition;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
