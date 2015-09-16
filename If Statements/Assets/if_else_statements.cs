8f5ca75aff58f7ebd6b523b4befde98981c63fba
Derrick Nozumi <10596306@uvu.edu>
2015-09-16 15:55:15 -0600

    If statements files

---
+++ If Statements/Assets/if_else_statements.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class if_else_statements : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	float altitude = 200;
+	float altitudeheightlimit= 100;
+	float altitudedepthlimit = 50;
+	
+	
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			AltitudeTest();
+		
+		altitude -= Time.deltaTime * 50;
+	}
+	
+	
+	void AltitudeTest ()
+	{
+	if (altitude > altitudeheightlimit)
+		{
+			print ("you are too High");
+	}
+	else if(altitude < altitudedepthlimit)
+	{
+		print ("you are too low");
+}
+		else 
+		{
+			print ("you are flying stable");
+		}
+	}
+}
---
+++ If Statements/Assets/if_statements.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class if_statements : MonoBehaviour {
+
+	// Use this for initialization
+	void Start (){
+	}
+		float altitude = 30;
+		float altitudeheightlimit= 50;
+		float altitudedepthlimit = 10;
+		
+		
+		void Update ()
+		{
+			if(Input.GetKeyDown(KeyCode.Space))
+				AltitudeTest();
+			
+			altitude -= Time.deltaTime * 50;
+		}
+		
+		
+		void AltitudeTest ()
+		{
+
+			if(altitude > altitudeheightlimit)
+			{
+
+				print("You are too High.");
+			}
+	
+			else if(altitude < altitudedepthlimit)
+			{
+			
+				print("You are too low.");
+			}
+
+			else
+			{
+			
+				print("Flight path Stable.");
+			}
+		}
+}
