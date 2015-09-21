e92badd4283eb08e60859de0316495d849224a08
Derrick Nozumi <10596306@uvu.edu>
2015-09-21 16:10:07 -0600

    Loops files for assignment

---
+++ Loops/Assets/Loops.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour 
+
+ {
+	int numCars = 6;
+
+	void Start () 
+	{
+		for(int i = 0; i < numCars; i++)
+		{
+			Debug.Log("Creating Additional Transport: "+i);
+	}
+	}
+}
+
+/*
+//---------------------------------
+
+{
+	int choresToDo = 8;	
+
+	void Start ()
+	{
+		while(choresToDo > 0)
+		{
+			Debug.Log ("I've finished a Chore");
+			choresToDo--;
+	}
+	}
+}
+*/
+
+
+
+	//----------------------------
+/*
+{
+	
+
+	void Start () {
+		bool shouldContinue = false;
+
+		do 
+		{
+					print ("All your base are belong to us");
+				} while(shouldContinue == true);
+	}
+}
+
+
+
+
+*/
+
+
+	// -----------------------
+/*
+{
+	void Start () 
+{
+		string[] strings = new string[4];
+
+		strings [0] = "Bueller?";
+		strings [1] = "Bueller???";
+		strings [2] = "Is Bueller here";
+		strings [3] = "Come on now Bueller....";
+
+		foreach(string item in strings)
+		{
+			print (item);
+
+	}
+	}
+}
+
+*/
+
---
+++ Loops/Assets/Loops_passoff.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops_passoff : MonoBehaviour
+{
+	void Start()
+	{
+	string[] strings = new string[4];
+
+	strings [0] = "Bueller?";
+	strings [1] = "Bueller???";
+	strings [2] = "Is Bueller here";
+	strings [3] = "Come on now Bueller....";
+
+		foreach (string item in strings) {
+			print (item);
+		}
+	}
+
+}
