56d8481420c2c0d6e2345cfe2640441539d28b34
Derrick Nozumi <10596306@uvu.edu>
2015-10-12 16:12:02 -0600

    Classes Github Push

---
+++ Classes/Assets/Classes.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Classes : MonoBehaviour {
+
+
+	public class Resources
+	{
+		public int gold;
+		public int wood;
+		public int food;
+
+		public Resources(int gol, int woo, int foo)
+		{
+			gol = 5000;
+			gold = gol;
+			wood = woo;
+			food = foo;
+		}
+	}
+
+
+		public Resources baseResources = new Resources(200, 500, 900);
+
+
+
+
+
+
+	void Start () {
+		
+		Debug.Log (baseResources.food);
+
+		int first = 10;
+		int second = first;
+		second = 100;
+
+		Resources newResources = baseResources;
+		newResources.food = 0;
+
+		Transform trans1 = transform;
+		Transform trans2 = trans1;
+	}
+}
+
