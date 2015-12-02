7270b3d6513b7bbe4ae9212d83e53e16b43ca92b
Derrick Nozumi <10596306@uvu.edu>
2015-12-02 15:17:17 -0700

    Duck Hunt Gameobject creation data

---
+++ Duck Hunt Game copy/Assets/Scripts/Direction_Changer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Direction_Changer : MonoBehaviour {
+
+	public enum Changer{ Horizontal, Vertical};
+	public Changer changer;
+
+	void OnCollisionEnter(Collision hit)
+	{
+	Duck_movement  movement = hit.gameObject.GetComponent<Duck_movement>();
+	if(hit.transform.tag == "Duck")
+	{
+		if(changer == Changer.Horizontal)
+		{
+				movement.DirectionChanger(new Vector3(-1,1,0));
+		}
+		else if (changer == Changer.Vertical)
+		{
+				movement.DirectionChanger(new Vector3(1,-1,0));
+		}
+	 }
+
+   }
+
+}
---
+++ Duck Hunt Game copy/Assets/Scripts/Duck_Health.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Duck_Health : MonoBehaviour {
+
+
+
+	/*	void Start () {	
+	}
+	
+
+	void Update () {
+	
+	}*/
+	void OnTriggerEnter(Collider hit)
+	{
+		if (hit.tag == "Killzone")
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
